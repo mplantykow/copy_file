4aff5e32147edb1b87e2f8d7c92968fba4cb7c8f
mplantykow <m.plantykow@gmail.com>
2022-03-04 17:30:32 +0100

    copy_file: Add class Descritor it's queue mechanism
    Add Descriptor class, that is created to store data
    buffers and queueing mechanism, in which created descriptors
    are stored.
    Move read and write functionallities to separate functions.
    This approach makes reading and writing operations less dependent
    from each other.
    
    Signed-off-by: mplantykow <m.plantykow@gmail.com>

--- copy_file.cpp
+++ copy_file.cpp
@@ -5,15 +5,59 @@
 #include <filesystem>
 #include <string>
 #include "md5imp.h"
+#include <queue>
 
 #define SUCCESS 0
+
+#define BUFF_SIZE 4096
+
+class Descriptor
+{
+	public:
+		char data[BUFF_SIZE];
+		int size;
+		bool last;
+		Descriptor(char p[BUFF_SIZE], int size, bool last);
+};
+
+Descriptor::Descriptor(char p[BUFF_SIZE], int size, bool last)
+{
+	memcpy(this->data, p, BUFF_SIZE);
+	this->size = size;
+	this->last = last;
+}
+
+std::queue<Descriptor> queue;
+
+void file_read(std::ifstream* in)
+{
+	do{
+		char data[BUFF_SIZE];
+		in->read(data, BUFF_SIZE);
+		int size = in->gcount();
+		bool last = !*in;
+		queue.emplace(data, size, last);
+	} while(*in);
+}
+
+void file_write(std::ofstream* out)
+{
+	bool last = false;
+	do {
+		bool is_empty = queue.empty();
+		if (!is_empty) {
+			Descriptor pwrite = queue.front();
+			last = pwrite.last;
+			out->write(pwrite.data, pwrite.size);
+			queue.pop();
+		}
+	} while (!last);
+}
 
 int main(int argc, char *argv[])
 {
 	typedef std::chrono::high_resolution_clock Time;
 	typedef std::chrono::duration<float> fsec;
-	static constexpr std::size_t buff_size{4096};
-	char buffer[buff_size];
 
 	if (argc != 3) {
 		std::cout << "usage: copy_file <src-file> <dest-file>" << std::endl;
@@ -39,11 +83,8 @@ int main(int argc, char *argv[])
 
 	auto t_start = Time::now();
 
-	while (in.read(buffer, buff_size)) {
-		out.write(buffer, buff_size);
-	}
-
-	out.write(buffer, in.gcount());
+	file_read(&in);
+	file_write(&out);
 
 	auto t_end = Time::now();
 
